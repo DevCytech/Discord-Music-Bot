808b1644f8db3ac21379d2878a7e222da54f47b1
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-03-13 13:52:30 -0500

    EDIT: VoiceStateUpdate
    - Formatting edits
    - The bot can no longer be left alone
    - The bot will now delete the queue if the bot is disconnected and playing
    - The bot will try leaving a voice channel if in one and not playing

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,11 +1,46 @@
-const { client } = require('../client');
+const { client } = require('../index');
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
+	// Check if there is a queue
 	const queue = client.queue.get(newMember.guild.id);
-		
-	// If bot has moved channels
-	if (newMember.user.id === client.user.id && newMember.voice.channel !== queue.voiceChannel) {
-		queue.voiceChannel = newMeber.voice.channel;
-		client.queue.set(newMember.guild.id, queue);
+	if (!queue) {
+		if (newMember.guild.me.voice.channelID) {
+			return newMember.guild.me.voice.channel.leave();
+		} else {
+			return;
+		}
 	}
-}
+
+	// Create channel variables
+	const channel = newMember.channelID
+		? newMember.guild.channels.cache.get(newMember.channelID)
+		: newMember.guild.channels.cache.get(oldMember.channelID)
+		? newMember.guild.channels.cache.get(oldMember.channelID)
+		: null;
+
+	// Make sure these are events to do with the bot
+	if (newMember.id == client.user.id) {
+		// Check if bot was disconnected
+		if (!newMember.channelID) {
+			queue.textChannel.send(
+				'It seems I have been disconnected. I will now stop playing...',
+			);
+			newMember.guild.me.voice.channel.leave();
+			return client.queue.delete(newMember.guild.id);
+		}
+
+		// Check if bot moved channels
+		if (newMember.channelID !== queue.voiceChannel.id) {
+			queue.voiceChannel = channel;
+		}
+	}
+
+	// Check if bot was left alone
+	if (channel.members.size <= 1 && queue.textChannel.guild.me.voice.channel) {
+		queue.textChannel.send(
+			'It seems there are no people listening. I will now stop playing...',
+		);
+		queue.textChannel.guild.me.voice.channel.leave();
+		return client.queue.delete(queue.textChannel.guild.id);
+	}
+});
