3cc54fa7c09cc49aa7cf19eade8191640249be87
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-03-19 09:51:48 -0400

    queue --> serverQueue

--- commands/volume.js
+++ commands/volume.js
@@ -25,13 +25,14 @@ module.exports.callback = async ({ client, message, args }) => {
 
 	// Check to see if argument is a filter
 	if (!args[0] || isNaN(args[0]) || parseInt(args[0], 10) > 100 || parseInt(args[0], 10) < 1) {
-		return message.reply(`Volume can only be changed between 1 and 100. Current Volume: ${queue.volume}%`);
+		return message.reply(`Volume can only be changed between 1 and 100. Current Volume: ${serverQueue.volume}%`);
 	}
 
 	// Change volume
-  queue.volume = parseInt(args[0], 10);
-	serverQueue.dispatcher.setVolumeLogarithmic(queue.volume / 100);
-  return message.channel.send(`Volume has been set to ${queue.volume}%`);
+  	serverQueue.volume = parseInt(args[0], 10);
+	client.queue.set(message.guild.id, serverQueue);
+	serverQueue.dispatcher.setVolumeLogarithmic(serverQueue.volume / 100);
+  return message.channel.send(`Volume has been set to ${serverQueue.volume}%`);
 };
 
 module.exports.config = {
