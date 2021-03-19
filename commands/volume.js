8df46993690a1cccc33fbbadd73ce968df0cc272
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-03-19 09:38:01 -0400

    Created a volume command.

---
+++ commands/volume.js
@@ -0,0 +1,41 @@
+module.exports.callback = async ({ client, message, args }) => {
+	// Check voice channel
+	const channel = message.member.voice.channel;
+	if (!channel) {
+		return message.reply(
+			'Please join a voice channel to use this command.',
+		);
+	}
+
+	// Make sure the bot if in a voice channel
+	if (!message.guild.me.voice.channel) {
+		return message.reply('I am not currently playing music!');
+	}
+
+	// Make sure they are in the same voice channel
+	if (channel.id !== message.guild.me.voice.channel.id) {
+		return message.reply(
+			'Please join the same voice channel as me to use this command.',
+		);
+	}
+
+	// Get the server queue
+	const serverQueue = client.queue.get(message.guild.id);
+	if (!serverQueue) return message.reply('There is nothing playing.');
+
+	// Check to see if argument is a filter
+	if (!args[0] || isNaN(args[0]) || parseInt(args[0], 10) > 100 || parseInt(args[0], 10) < 1) {
+		return message.reply(`Volume can only be changed between 1 and 100. Current Volume: ${queue.volume}%`);
+	}
+
+	// Change volume
+  queue.volume = parseInt(args[0], 10);
+	serverQueue.dispatcher.setVolumeLogarithmic(queue.volume / 100);
+  return message.channel.send(`Volume has been set to ${queue.volume}%`);
+};
+
+module.exports.config = {
+	name: 'volume',
+	aliases: ['vol'],
+	category: 'music',
+};
